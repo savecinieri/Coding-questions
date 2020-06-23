324769818d3ef79fcdc1511c666c86bc6ff66bfb
savecinieri <s239214@studenti.polito.it>
2020-06-23 11:43:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CodingQuestion7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] val = {2, 4, 6, 2, 5};
+
+            Console.WriteLine("Result: " + function(val));
+        }
+
+        static int function(int[] val)
+        {
+            int maxSum = int.MinValue;
+
+            for(var i = 0; i < val.Length - 1; i++)
+            {
+                for(var j = 2;  i + j < val.Length; j++)
+                {
+                    int tmpSum = val[i];
+                    for(var z = i + j; z < val.Length; z += j)
+                    {
+                        tmpSum += val[z];
+                    }
+
+                    if (tmpSum > maxSum)
+                    {
+                        //Console.WriteLine("TmpSum: " + tmpSum);
+                        maxSum = tmpSum;
+                    }
+                }
+            }
+
+            return maxSum;
+        }
+
+    }
+}
